79d5e78d517dc53b5a6f3615bf3cb561790f5456
kmatyaszek <kazimierz.matyaszek@gmail.com>
2019-11-04 21:37:03 +0100

    Initial commit

---
+++ Controllers/FilesController.cs
@@ -0,0 +1,23 @@
+﻿using System.IO;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetCoreAuthorizeStaticFile.Controllers
+{
+    public class FilesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Authorize]
+        public IActionResult GetSecretDocument()
+        {
+            var file = Path.Combine(Directory.GetCurrentDirectory(),
+                "SecureFiles", "secret.pdf");
+
+            return PhysicalFile(file, "application/pdf");
+        }
+    }
+}
